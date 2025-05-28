5eaaf2643c5c2e72cd147f73b5a24dc70bd843b4
maddie w <madeleine.w_2023_lon@outlook.com>
2025-05-28 21:34:06 +0100

    basic layout

---
+++ SEA-React/src/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+
+  return (
+    <>
+    <p>username</p>
+    <input></input>
+    <p>password</p>
+    <input></input>
+    <button>Login</button>
+    </>
+  )}
+
+  export default Login
